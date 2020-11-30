14fc77a0281a7a416150d353a6137297d82b9761
Deepika <deepika.kattel80@gmail.com>
2020-11-30 16:14:37 +0545

    Added logo and made necessary changes

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,10 +1,10 @@
 
 <!---Top Menu--->
-    <div id="cssmenu">    
+    <div id="cssmenu" >    
         <ul>           
-        <img src="{{asset('images/logoFull.png')}}" style="width:140px;height:71px">
+        <img src="{{asset('images/logo_original.png')}}">
            {{--<li class="has-sub"><a href="#"><span>Category</span></a>
-              <ul>
+              <ul >
                  <li class="has-sub"><a href="#"><span>Item 1</span></a>
                     <ul>
                        <li><a href="#"><span>Sub Item</span></a></li>
@@ -20,11 +20,11 @@
               </ul>
            </li>--}}
            <!-- <li><a href="#about"><span>about</span></a></li> -->
-           <li><a href="{{route('career.index')}}"><span>Career</span></a></li>
+           <li><a href="{{route('career.index')}}" style="font-size:15px;"><span>Career</span></a></li>
            <!-- <li><a href="{{url('/')}}"><img src="{{asset('images/logo.png')}}" style="width:200px; height:100px;margin-bottom:30px;background:transparent"></a></li> -->
-           <li><a href="{{route('vision')}}"><span>Vision</span></a></li>
-           <li class="last"><a href="{{route('contact')}}"><span>Contact</span></a></li>
-           <li class="active"><a href="{{url('/')}}"><span>Let IT Grow</span></a></li>
+           <li><a href="{{route('vision')}}"  style="font-size:15px;"><span>Vision</span></a></li>
+           <li class="last"><a href="{{route('contact')}}" style="font-size:15px;"><span>Contact</span></a></li>
+           <li class="active"><a href="{{url('/')}}" style="font-size:15px;"><span>Let IT Grow</span></a></li>
            <!-- @if (Route::has('login'))
                @auth
                     <li><a href="{{ url('/home') }}"><span>{{Auth::user()->name}}</span></a></li>
